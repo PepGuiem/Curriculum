d480508b0ceacb831f5f6ba71c2afe23f5781246
PepGuiem <82808082+PepGuiem@users.noreply.github.com>
2023-10-29 20:24:18 +0100

    only-curriculum started

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,10 @@ document.getElementById("menu").addEventListener("click", function() {
     var menuContent = this.querySelector(".dropdown-content");
     menuContent.classList.toggle("active");
 });
+
+document.getElementById("only-curriculum").addEventListener("click", function(){
+    var principal = document.querySelector(".div-content");
+    var onlyCurriculun = document.querySelector(".div-content2");
+    principal.classList.toggle("desactive");
+    onlyCurriculun.classList.toggle("active");
+});
