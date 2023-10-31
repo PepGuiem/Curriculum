540eede90fed354177636544d9c5ba9e7cb6db2a
Pep Guiem <pepguiemnolla@gmail.com>
2023-10-31 20:30:55 +0100

    cositas

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,58 @@
+var principal = document.querySelector(".lamp-content1");
+var principal2 = document.querySelector(".game")
+var onlyCurriculun = document.querySelector(".lamp-onlyCurriculum");
+let controladorToggle = 0;
+let pageCurriculum = 0;
+var curriculum1 = document.querySelector(".div-content-onlyCurriculum.content1");
+var curriculum2 = document.querySelector(".div-content-onlyCurriculum.content2");
+var curriculum3 = document.querySelector(".div-content-onlyCurriculum.content3");
+var next = document.getElementById("next");
+
 document.getElementById("menu").addEventListener("click", function() {
     var menuContent = this.querySelector(".dropdown-content");
     menuContent.classList.toggle("active");
 });
 
 document.getElementById("only-curriculum").addEventListener("click", function(){
-    var principal = document.querySelector(".div-content");
-    var onlyCurriculun = document.querySelector(".div-content2");
     principal.classList.toggle("desactive");
+    principal2.classList.toggle("desactive");
     onlyCurriculun.classList.toggle("active");
+    if(controladorToggle == 1){
+        principal.classList.toggle("active");
+        principal2.classList.toggle("active");
+        onlyCurriculun.classList.toggle("desactive");
+        controladorToggle--;
+    }
+});
+
+document.getElementById("back").addEventListener("click", function(){
+    pageCurriculum--;
+    if(pageCurriculum == -1){
+        principal.classList.toggle("desactive");
+        principal2.classList.toggle("desactive");
+        onlyCurriculun.classList.toggle("active");
+        principal.classList.toggle("active");
+        principal2.classList.toggle("active");
+        onlyCurriculun.classList.toggle("desactive");
+        controladorToggle++;
+    }else if(pageCurriculum == 0){
+        curriculum1.style.display = "flex";
+        curriculum2.style.display = "none";
+    } else if(pageCurriculum == 1){
+        curriculum2.style.display = "flex";
+        curriculum3.style.display = "none";
+        next.style.display = "block";
+    }
+});
+
+next.addEventListener("click", function(){
+    pageCurriculum++;
+    if(pageCurriculum == 1){
+        curriculum1.style.display = "none";
+        curriculum2.style.display = "flex";
+    }else if(pageCurriculum == 2){
+        curriculum2.style.display = "none";
+        curriculum2.style.display = "flex";
+        next.style.display = "none";
+    }
 });
